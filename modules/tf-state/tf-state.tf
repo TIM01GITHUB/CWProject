fe2903aee798fe4bebb2a558b9ac8d4bd83ec038
Aryabhata <142987366+TIM01GITHUB@users.noreply.github.com>
2023-10-24 21:39:59 +1100

    tf-state

--- modules/tf-state/tf-state.tf
+++ modules/tf-state/tf-state.tf
@@ -2,7 +2,6 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket        = var.bucket_name
   force_destroy = true
 }
-
 resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
